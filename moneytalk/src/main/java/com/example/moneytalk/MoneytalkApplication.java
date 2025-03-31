bda5338b9de8d6241ac76592193ef96736e0b844
dohyundaniel <kdhdaniel0506@gmail.com>
2025-04-01 05:00:45 +0900

    initial commit

---
+++ moneytalk/src/main/java/com/example/moneytalk/MoneytalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.moneytalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneytalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneytalkApplication.class, args);
+	}
+
+}
---
+++ moneytalk/src/test/java/com/example/moneytalk/MoneytalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moneytalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneytalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
