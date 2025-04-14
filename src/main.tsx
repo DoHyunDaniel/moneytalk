ed9a0f0e03b2f9f7e9ea3124aa330d4d9c4daa64
dohyundaniel <kdhdaniel0506@gmail.com>
2025-04-15 06:23:11 +0900

    🔧 프론트엔드 업데이트: 로그인/헤더/소셜로그인 동기화 완료

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import AppRouter from "./router/AppRouter";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import Header from "./components/Header";
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <Header />
+        <AppRouter />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
